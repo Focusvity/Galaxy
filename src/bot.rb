c48e6f18cb82be82fc72dd03962ace5e229879de
Nathan Curran <nathan.curran10012@gmail.com>
2019-12-10 20:45:46 +1100

    Oops

--- src/bot.rb
+++ src/bot.rb
@@ -5,9 +5,6 @@ require 'dotenv/load'
 
 # Main bot module
 module Bot
-  # Load modules
-  Dir['src/modules/*.rb'].each { |mod| load mod }
-
   BOT = Discordrb::Commands::CommandBot.new(token: ENV['TOKEN'],
                                             client_id: ENV['CLIENT_ID'],
                                             prefix: ENV['COMMAND_PREFIX'])
@@ -16,7 +13,7 @@ module Bot
     BOT.clear! if clear
     new_module = Module.new
     const_set(clazz.to_s, new_module)
-    Dir["src/modules/#{path}/*rb"].each { |file| load file }
+    Dir["src/modules/#{path}/*.rb"].each { |file| load file }
     new_module.constants.each do |mod|
       BOT.include! new_module.const_get(mod)
     end
@@ -27,4 +24,3 @@ module Bot
 
   BOT.run
 end
-
